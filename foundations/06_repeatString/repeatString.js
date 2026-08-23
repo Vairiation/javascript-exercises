08c1f044a173ea91a14d640fdb043ea2f37234ab
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-08-23 18:37:56 -0500

    Complete foundations exercises 6, 7, 8, and 9

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repetitions) {
+    let repeat = '';
+    if (repetitions < 0) return 'ERROR';
+    for (let i = 0; i < repetitions; i++) {
+        repeat += str;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
