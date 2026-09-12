b26adb4f2f1d3106bcf5139425216d56d2ecf0a1
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-09-12 16:56:44 -0500

    Complete foundations/13_palindromes/palindromes.js exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const filteredString = string
+        .toLowerCase()
+        .split('')
+        .filter(character => alphanumerical.includes(character))
+        .join('');
+    const reverse = filteredString
+        .split('')
+        .reverse()
+        .join('');
+    return filteredString === reverse;
 };
 
 // Do not edit below this line
