11360a67ec31f0ee067514664d0d805446a4f546
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-09-13 15:28:10 -0500

    Complete foundations/16_findTheOldest/findTheOldest.js exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+function getAge(birth, death, currentYear) {
+    return (death ?? currentYear) - birth;
+}
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    const array = people.slice().sort((a, b) => {
+        const aAge = getAge(a.yearOfBirth, a.yearOfDeath, currentYear);
+        const bAge = getAge(b.yearOfBirth, b.yearOfDeath, currentYear);
+        return aAge - bAge;
+    })
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
